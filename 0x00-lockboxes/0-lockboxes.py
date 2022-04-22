8328ed288c317e0e682243d34ca728fd0cd22397
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-04-22 17:14:01 +0000

    updated task 0 -  fixed IndexError

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,11 +9,12 @@ def canUnlockAll(boxes):
     """determines if all the boxes can be opened"""
     pending_boxes = [0]
     visited_boxes = []
-    while len(pending_boxes):
-        for key in boxes[pending_boxes[0]]:
-            if (isinstance(key, int) and
-               (key not in pending_boxes and key not in visited_boxes)):
-                pending_boxes.append(key)
+    while len(pending_boxes) > 0:
+        if (pending_boxes[0] < len(boxes)):
+            for key in boxes[pending_boxes[0]]:
+                if (isinstance(key, int) and
+                (key not in pending_boxes and key not in visited_boxes)):
+                    pending_boxes.append(key)
         visited_boxes.append(pending_boxes[0])
         pending_boxes.pop(0)
     for box in range(len(boxes)):
