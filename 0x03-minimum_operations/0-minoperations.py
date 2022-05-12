87d3d2371b9263489ce5328386179b7d626f4e3e
Maximiliano Pan <maximiliano.pan@hotmail.com>
2022-05-12 23:52:25 +0000

    added task 0

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""488#0 - Minimum Operations"""
+
+
+def minOperations(n):
+    """In a text file, there is a single character 'H'. 
+    Your text editor can execute only two operations in this file: 
+    'Copy All' and 'Paste'. 
+    Given a number n, write a method that calculates the fewest number 
+    of operations needed to result in exactly n 'H' characters in the file"""
+    pichu, div = 0, 2
+    while isinstance(n, int) and n > 1:
+        while n % div:
+            div += 1
+        pichu += div
+        n = int(n / div)
+    return pichu
